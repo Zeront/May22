eff157afe8d41a8882418328517028053804a681
Zeront <ncosta.zeront@gmail.com>
2015-05-22 11:40:56 +0100

    teste

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<htm>
+	<head>
+		<meta charset="utf-8"/>
+		<title>May22</title>
+	</head>
+	<body>
+		<footer></footer>
+	</body>
+</htm>
+
+<?php
+	$myName = "Nelson";
+	echo $myName;
+?>
