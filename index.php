297617c24844391e4bdfd2265acb16ff25d975e0
Zeront <ncosta.zeront@gmail.com>
2015-05-22 12:39:53 +0100

    Setting up Head

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <htm>
 	<head>
-		<meta charset="utf-8"/>
 		<title>May22</title>
+		<meta charset="utf-8"/>
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+		<meta name="description" content="Personal info and contact page."/>
+		<meta name="author" content="Nelson Costa, @Zeront"/>
+		<meta name="copyright" content="(c) Copyright Nelson Costa 2015."/>
 	</head>
 	<body>
 		<footer></footer>
