0cf213fca33134d85929a691c70ce6712f7d83a4
Zeront <ncosta.zeront@gmail.com>
2015-05-22 12:46:59 +0100

    Import Foundation Framework

--- index.php
+++ index.php
@@ -8,9 +8,20 @@
 		<meta name="description" content="Personal info and contact page."/>
 		<meta name="author" content="Nelson Costa, @Zeront"/>
 		<meta name="copyright" content="(c) Copyright Nelson Costa 2015."/>
+		<!--Import foundation.css-->
+		<link rel="stylesheet" href="assets/css/foundation.css"/>
+		<!--Import modernizr.js-->
+		<script src="assets/js/vendor/modernizr.js"></script>
 	</head>
 	<body>
 		<footer></footer>
+		<!--Import jquery.js-->
+		<script src="assets/js/vendor/jquery.js"></script>
+		<!--Import foundation.min.js-->
+		<script src="assets/js/foundation.min.js"></script>
+		<script>
+			$(document).foundation();
+		</script>
 	</body>
 </htm>
 
